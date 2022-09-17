1c6e7500b9d38f62e3f6c2d254a046dc2706f07e
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2022-09-17 11:21:55 -0300

    usando o mesmo método de https://github.com/lalyos/jfiglet

---
+++ src/main/java/julian/gamboa/herokuSetembro/HerokuSetembroApplication.java
@@ -0,0 +1,22 @@
+package julian.gamboa.herokuSetembro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HerokuSetembroApplication {
+
+	
+	@RequestMapping("/")
+	public String home() {
+	  return "Hello   2022";
+	}
+  
+	public static void main(String[] args) {
+	  SpringApplication.run(HerokuSetembroApplication.class, args);
+	}
+
+}
